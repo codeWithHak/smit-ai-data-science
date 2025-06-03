1628bab7b98360c8bdee9aa786b0ad9f8ebb764b
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-06-03 14:23:48 +0500

    complete practice assignment 1

---
+++ 01-module-python/assignments/mandatory_assignments/02_assignment.py
@@ -0,0 +1,36 @@
+from math import pi
+
+# 1 Area of a Rectangle:
+
+"""
+def calculate_area(length,width):
+    area = length * width
+    return area
+res = calculate_area(10,20)
+print(res) 
+"""
+
+# 2 Circumference of a Circle:
+
+"""def circumference(radius):
+    return 2 * pi * radius
+print(circumference(10))"""
+
+# 3 Simple Interest:
+"""
+def simple_interest(principal,time,rate):
+    return principal * time * rate
+print(simple_interest(10,10,10))"""
+ 
+# 4 Speed of an object
+
+def speed(distance,time):
+    return distance / time
+print(speed(10,2))
+
+# 5 BMI Calculator
+
+def bmi_calc(weight,height):
+    return weight / height**2
+
+print(bmi_calc(50,5.8))
